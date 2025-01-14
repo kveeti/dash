da465b2dcafdf348a3f7826c44ef06480feb6d68
Veeti K <veeti@veetik.com>
2025-01-14 23:45:23 +0200

    return categories without query as well

--- back/src/data/categories.ts
+++ back/src/data/categories.ts
@@ -4,11 +4,13 @@ import type { Category } from "./transactions.ts";
 export function categories(sql: Pg) {
 	return {
 		query: async ({ userId, query }: { userId: string; query: string }) => {
+			const filter = query ? sql`and lower(c.name) like ${query}` : sql``;
+
 			const rows: Array<Category> = await sql`
 				select id, name
 				from transaction_categories c
 				where c.user_id = ${userId}
-				and lower(c.name) like ${query}
+				${filter}
 				limit 20;
 			`;
 
