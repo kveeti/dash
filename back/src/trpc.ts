69f203342f61c04958b8a2506a03a0ccf0eca103
Veeti K <veeti@veetik.com>
2025-02-02 15:16:01 +0200

    basic request logging

--- back/src/trpc.ts
+++ back/src/trpc.ts
@@ -17,7 +17,25 @@ export function createContext(data: Data, services: { auth: Auth }) {
 export type Context = Awaited<ReturnType<typeof createContext>>;
 
 export const router = t.router;
-export const publicProcedure = t.procedure;
+
+export const publicProcedure = t.procedure.use(async (opts) => {
+	const start = Date.now();
+	const result = await opts.next();
+	const durationMs = Date.now() - start;
+
+	const now = new Date();
+	const meta = {
+		ts: now.toISOString(),
+		path: opts.path,
+		type: opts.type,
+		durationMs,
+		ok: result.ok,
+	};
+
+	console[result.ok ? "log" : "error"](meta);
+
+	return result;
+});
 
 export const authProc = publicProcedure.use(async (opts) => {
 	const shouldConsiderCsrf = opts.ctx.info.type === "mutation";
