bd67bf0f407ff99823ce94d54db4337aa6fc189a
Veeti K <veeti@veetik.com>
2025-02-12 01:26:45 +0200

    maybe fix migrations

--- back/src/data/data.ts
+++ back/src/data/data.ts
@@ -29,7 +29,7 @@ export type Pg = ReturnType<typeof postgres>;
 export type Data = Awaited<ReturnType<typeof getData>>;
 
 async function migratePg(pg: Pg) {
-	const script_1 = (pg: Pg) =>
+	const script_0 = (pg: Pg) =>
 		pg`
 create table users (
 	id varchar(30) primary key not null,
@@ -107,12 +107,12 @@ create table if not exists transactions_tags (
 create index if not exists idx_transactions_tags_user_id on transactions_tags(user_id);
 `.simple();
 
-	const script_2 = (pg: Pg) =>
+	const script_1 = (pg: Pg) =>
 		pg`
 alter table transaction_categories add column is_neutral boolean not null default false;
 `.simple();
 
-	const script_3 = (pg: Pg) =>
+	const script_2 = (pg: Pg) =>
 		pg`
 create table user_preferences (
 	user_id varchar(30) primary key not null,
@@ -125,7 +125,7 @@ create table user_preferences (
 );
 `.simple();
 
-	const script_4 = (pg: Pg) =>
+	const script_3 = (pg: Pg) =>
 		pg`
 create table if not exists transactions_links (
 	id varchar(30) not null primary key,
@@ -143,33 +143,31 @@ create unique index on transactions_links (transaction_a_id, transaction_b_id);
 create unique index on transactions_links (transaction_b_id, transaction_a_id);
 `.simple();
 
-	const scripts = [script_1, script_2, script_3, script_4];
+	const scripts = [script_0, script_1, script_2, script_3];
 
 	await pg.begin(async (t) => {
 		await t`create table if not exists __version (v int);`;
-		let version = 1;
+		let version = 0;
 		const [row]: [{ v: number }?] = await t`select v from __version limit 1;`;
 		if (!row) {
-			await t`insert into __version (v) values (1);`;
+			await t`insert into __version (v) values (0);`;
 		} else {
 			version = row.v;
 		}
 
-		if (version >= scripts.length) {
+		if (version == scripts.length - 1) {
 			console.log("no migrations to run", { version });
 			return;
 		}
 
-		let latestVersion = 1;
-		for (let i = 0; i < scripts.length; i++) {
-			const newVersion = i + 1;
-
+		let latestVersion = 0;
+		for (let i = version === 0 ? version : version + 1; i < scripts.length; i++) {
 			const script = scripts[i];
 			await script(t);
-			await t`update __version set v = ${newVersion}`;
-			latestVersion = newVersion;
+			await t`update __version set v = ${i}`;
+			latestVersion = i;
 		}
 
-		console.log("migrated db to version", latestVersion);
+		console.log("migrated db from", version, "to", latestVersion);
 	});
 }
