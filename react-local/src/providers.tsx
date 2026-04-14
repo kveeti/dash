b0a98babd2a2fbb567e93c5211dd52dd68948fa8
Veeti K <veeti@veetik.com>
2026-04-15 01:10:33 +0300

    sync after changes

--- react-local/src/providers.tsx
+++ react-local/src/providers.tsx
@@ -1,137 +1,147 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+	MutationCache,
+	QueryClient,
+	QueryClientProvider,
+} from "@tanstack/react-query";
 import { createContext, useContext, useMemo, type ReactNode } from "react";
 import { getDb } from "./lib/db";
 import { getHLCGenerator } from "./lib/hlc";
+import { queryKeyRoots } from "./lib/queries/query-keys";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+	mutationCache: new MutationCache({
+		onSuccess: () => {
+			queryClient.invalidateQueries({
+				queryKey: queryKeyRoots.sync,
+				exact: false,
+			});
+		},
+	}),
+});
 
 export function Providers(props: { children: ReactNode }) {
-  return (
-    <I18nProvider>
-      <DbProvider>
-        <QueryClientProvider client={queryClient}>
-          {props.children}
-        </QueryClientProvider>
-      </DbProvider>
-    </I18nProvider>
-  );
+	return (
+		<I18nProvider>
+			<DbProvider>
+				<QueryClientProvider client={queryClient}>
+					{props.children}
+				</QueryClientProvider>
+			</DbProvider>
+		</I18nProvider>
+	);
 }
 
 const DbContext = createContext<ReturnType<typeof getDb> | null>(null);
 const clientId = "client-1";
 
 function DbProvider(props: { children: ReactNode }) {
-  const db = useMemo(() => getDb({ hlc: getHLCGenerator(clientId) }), []);
+	const db = useMemo(() => getDb({ hlc: getHLCGenerator(clientId) }), []);
 
-  return (
-    <DbContext.Provider value={db}>
-      {props.children}
-    </DbContext.Provider>
-  );
+	return <DbContext.Provider value={db}>{props.children}</DbContext.Provider>;
 }
 
 export function useDb() {
-  const context = useContext(DbContext);
-  if (!context) throw new Error("useDb must be used within a DbProvider!");
-  return context;
+	const context = useContext(DbContext);
+	if (!context) throw new Error("useDb must be used within a DbProvider!");
+	return context;
 }
 
 const I18NContext = createContext<ReturnType<typeof useI18nValue> | null>(null);
 
 export function useI18n() {
-  const context = useContext(I18NContext);
-  if (!context) throw new Error("useI18n must be used within a I18nProvider!");
-  return context;
+	const context = useContext(I18NContext);
+	if (!context) throw new Error("useI18n must be used within a I18nProvider!");
+	return context;
 }
 
 export function I18nProvider({ children }: { children: ReactNode }) {
-  const value = useI18nValue();
-  return <I18NContext.Provider value={value}>{children}</I18NContext.Provider>;
+	const value = useI18nValue();
+	return <I18NContext.Provider value={value}>{children}</I18NContext.Provider>;
 }
 
 function useI18nValue() {
-  const locale = "fi-FI";
-  const timeZone = "Europe/Helsinki";
-  const hourCycle: 12 | 24 = 24;
+	const locale = "fi-FI";
+	const timeZone = "Europe/Helsinki";
+	const hourCycle: 12 | 24 = 24;
 
-  const resolvedOptions = useMemo(
-    () => new Intl.DateTimeFormat(locale, { timeZone }).resolvedOptions(),
-    [locale, timeZone]
-  );
+	const resolvedOptions = useMemo(
+		() => new Intl.DateTimeFormat(locale, { timeZone }).resolvedOptions(),
+		[locale, timeZone],
+	);
 
-  const amountFormatter = useMemo(
-    () =>
-      new Intl.NumberFormat(locale, {
-        signDisplay: "auto",
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
-        currencyDisplay: "symbol",
-        style: "currency",
-        currency: "EUR",
-      }),
-    [locale]
-  );
+	const amountFormatter = useMemo(
+		() =>
+			new Intl.NumberFormat(locale, {
+				signDisplay: "auto",
+				minimumFractionDigits: 2,
+				maximumFractionDigits: 2,
+				currencyDisplay: "symbol",
+				style: "currency",
+				currency: "EUR",
+			}),
+		[locale],
+	);
 
-  const shortDateFormatter = useMemo(
-    () =>
-      new Intl.DateTimeFormat(locale, {
-        month: "numeric",
-        day: "numeric",
-      }),
-    [locale]
-  );
+	const shortDateFormatter = useMemo(
+		() =>
+			new Intl.DateTimeFormat(locale, {
+				month: "numeric",
+				day: "numeric",
+			}),
+		[locale],
+	);
 
-  const longDateFormatter = useMemo(
-    () =>
-      new Intl.DateTimeFormat(locale, {
-        month: "numeric",
-        day: "numeric",
-        year: "2-digit",
-      }),
-    [locale]
-  );
+	const longDateFormatter = useMemo(
+		() =>
+			new Intl.DateTimeFormat(locale, {
+				month: "numeric",
+				day: "numeric",
+				year: "2-digit",
+			}),
+		[locale],
+	);
 
-  const weekdayLongDateFormatter = useMemo(
-    () =>
-      new Intl.DateTimeFormat(locale, {
-        month: "numeric",
-        day: "numeric",
-        year: "numeric",
-        weekday: "short",
-      }),
-    [locale]
-  );
+	const weekdayLongDateFormatter = useMemo(
+		() =>
+			new Intl.DateTimeFormat(locale, {
+				month: "numeric",
+				day: "numeric",
+				year: "numeric",
+				weekday: "short",
+			}),
+		[locale],
+	);
 
-  const weekdayShortDateFormatter = useMemo(
-    () =>
-      new Intl.DateTimeFormat(locale, {
-        month: "numeric",
-        day: "numeric",
-        weekday: "short",
-      }),
-    [locale]
-  );
+	const weekdayShortDateFormatter = useMemo(
+		() =>
+			new Intl.DateTimeFormat(locale, {
+				month: "numeric",
+				day: "numeric",
+				weekday: "short",
+			}),
+		[locale],
+	);
 
-  const countFormatter = useMemo(
-    () =>
-      new Intl.NumberFormat(undefined, {
-        notation: "compact",
-        maximumFractionDigits: 1,
-      }),
-    [locale]
-  );
+	const countFormatter = useMemo(
+		() =>
+			new Intl.NumberFormat(undefined, {
+				notation: "compact",
+				maximumFractionDigits: 1,
+			}),
+		[locale],
+	);
 
-  return {
-    f: {
-      amount: amountFormatter,
-      shortDate: shortDateFormatter,
-      longDate: longDateFormatter,
-      weekdayLongDate: weekdayLongDateFormatter,
-      weekdayShortDate: weekdayShortDateFormatter,
-      count: countFormatter,
-    },
-    hourCycle,
-    timeZone: timeZone ?? resolvedOptions.timeZone,
-    locale: locale ?? resolvedOptions.locale,
-  };
+	return {
+		f: {
+			amount: amountFormatter,
+			shortDate: shortDateFormatter,
+			longDate: longDateFormatter,
+			weekdayLongDate: weekdayLongDateFormatter,
+			weekdayShortDate: weekdayShortDateFormatter,
+			count: countFormatter,
+		},
+		hourCycle,
+		timeZone: timeZone ?? resolvedOptions.timeZone,
+		locale: locale ?? resolvedOptions.locale,
+	};
 }
